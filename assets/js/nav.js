80991c08eef5ef266856cc7b3c59cdcdb3366540
pheonix <lukethemodded@gmail.com>
2019-06-05 00:50:16 +0100

    kkk

---
+++ assets/js/nav.js
@@ -0,0 +1,51 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+function openNav() {
+  document.getElementById("mySidenav").style.width = (window.innerWidth / 6) + "px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
+
+window.addEventListener('mousemove', function(e)
+{
+	if(window.innerWidth >= 330)
+	{
+		if(window.innerWidth <= 2000)
+		{
+			if(window.innerWidth <= 1080)
+			{
+				if(e.x <= (0 + (window.innerWidth / 4)))
+				{
+					openNav();
+				}
+				else
+				{
+				closeNav();
+				}
+			}
+			else
+			{
+				if(e.x <= (0 + (window.innerWidth / 6)))
+				{
+					openNav();
+				}
+				else
+				{
+				closeNav();
+				}
+			}
+		}
+		else
+		{
+			console.log("ERROR: Screen to big for right maths to take place");
+		}
+	}
+	else
+	{
+		console.log("ERROR: Screen to small for right maths to take place");
+	}
+});
